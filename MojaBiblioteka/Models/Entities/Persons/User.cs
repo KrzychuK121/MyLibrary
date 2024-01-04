a75da2eaf1a7d215fcb85450cf8b7e2f1c439535
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-04 14:02:32 +0100

    Adding FirstName and LastName

--- MojaBiblioteka/Models/Entities/Persons/User.cs
+++ MojaBiblioteka/Models/Entities/Persons/User.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace MojaBiblioteka.Models.Entities.Persons
 {
     public class User : IdentityUser
     {
+        [Display(Name = "Imię"), Required]
+        public Name FirstName { get; set; } = new Name { FirstName = string.Empty };
+        [Display(Name = "Nazwisko"), Required]
+        public LastName Surname { get; set; } = new LastName { Surname = string.Empty };
     }
 }
