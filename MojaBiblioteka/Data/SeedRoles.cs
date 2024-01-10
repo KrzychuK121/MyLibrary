d0e0c19237e62b90ec09b62452b872f0fe9b9135
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-10 11:27:17 +0100

    Created new Seed class who has to seed database with new roles (Admin and Employee) if they are missing. Changes are displayed in console (if any role is created or not).

---
+++ MojaBiblioteka/Data/SeedRoles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MojaBiblioteka.Models.Entities.Persons;
+
+namespace MojaBiblioteka.Data
+{
+    public class SeedRoles
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            bool ifRolesCreated = false;
+
+            string[] roles = { "Admin", "Employee" };
+
+            Console.WriteLine();
+            Console.WriteLine("------SeedRoles.Initialize()------");
+
+            foreach(var role in roles)
+            {
+                
+                if (roleManager.RoleExistsAsync(role).Result)
+                    continue;
+
+                ifRolesCreated = true;
+                Console.WriteLine($"\nCreating role {role}\n");
+
+                var identityRole = new IdentityRole(role);
+                var result = roleManager.CreateAsync(identityRole).Result;
+                if (!result.Succeeded)
+                    throw new ApplicationException($"Error while creating the {role} role");
+
+            }
+            if (!ifRolesCreated)
+                Console.WriteLine("\nNo roles created.\n");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
