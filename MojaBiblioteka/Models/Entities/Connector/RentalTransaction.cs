2899125bdc6f28554f84ac0063e33e61a00c46f4
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-12 20:00:36 +0100

    Giving default values for this entity model

--- MojaBiblioteka/Models/Entities/Connector/RentalTransaction.cs
+++ MojaBiblioteka/Models/Entities/Connector/RentalTransaction.cs
@@ -10,20 +10,22 @@ namespace MojaBiblioteka.Models.Entities.Connector
     {
         public string BookIsbn { get; set; }
         public string UserId { get; set; }
+        [Display(Name = "Książka")]
         public Book.Book Book { get; set; }
+        [Display(Name = "Użytkownik")]
         public User User { get; set; }
         [Required]
         [Display(Name = "Data wypożyczenia"), DataType(DataType.Date)]
-        public DateTime RentalDate { get; set; }
+        public DateTime RentalDate { get; set; } = DateTime.Now.Date;
         [Required]
         [Display(Name = "Data zwrotu"), DataType(DataType.Date)]
-        public DateTime DueDate { get; set; }
+        public DateTime DueDate { get; set; } = DateTime.Now.Date.AddMonths(3);
         [Required, Range(0, 3)]
         [Display(Name = "Możliwa ilość przedłużeń")]
         [Description("How many times can User prolong the term of the book return. Going from 3 to 0")]
         public int ProlongTermCounter { get; set; } = 3;
         [Required]
-        public int Status { get; set; } = 0; // BookStatus here
+        public int Status { get; set; } = (int)BookStatus.Ordered; // BookStatus here
 
     }
 
