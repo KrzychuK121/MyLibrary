61662391600b5d4051246673f9140067bf1feea5
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-12 19:47:23 +0100

    ViewModel for error/info/success informations displayed as alert on the side for user

---
+++ MojaBiblioteka/Models/ViewModels/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace MojaBiblioteka.Models.ViewModels.Messages
+{
+    public class Message
+    {
+        public string MessageText { get; set; } = string.Empty;
+        public int Type { get; set; } = (int) Types.Information;
+    }
+
+    public enum Types
+    {
+        Information,
+        Error,
+        Success
+    }
+}
