e2e1cd8c0ba82b768eb9f0aaa43e26664027d65c
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-13 11:44:32 +0100

    Migrations adding new pk field to RentalTransaction so it can be many records for the same book and user

--- MojaBiblioteka/Migrations/20240113104050_NewPKeyFieldInRentalTransaction.Designer.cs
+++ MojaBiblioteka/Migrations/20240113104050_NewPKeyFieldInRentalTransaction.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MojaBiblioteka.Data;
 
@@ -11,9 +12,10 @@ using MojaBiblioteka.Data;
 namespace MojaBiblioteka.Migrations
 {
     [DbContext(typeof(MyLibraryContext))]
-    partial class MyLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20240113104050_NewPKeyFieldInRentalTransaction")]
+    partial class NewPKeyFieldInRentalTransaction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MojaBiblioteka/Migrations/20240113104050_NewPKeyFieldInRentalTransaction.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MojaBiblioteka.Migrations
+{
+    public partial class NewPKeyFieldInRentalTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RentalTransactionList",
+                table: "RentalTransactionList");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RentalTransactionId",
+                table: "RentalTransactionList",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RentalTransactionList",
+                table: "RentalTransactionList",
+                column: "RentalTransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalTransactionList_BookIsbn",
+                table: "RentalTransactionList",
+                column: "BookIsbn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RentalTransactionList",
+                table: "RentalTransactionList");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RentalTransactionList_BookIsbn",
+                table: "RentalTransactionList");
+
+            migrationBuilder.DropColumn(
+                name: "RentalTransactionId",
+                table: "RentalTransactionList");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RentalTransactionList",
+                table: "RentalTransactionList",
+                columns: new[] { "BookIsbn", "UserId" });
+        }
+    }
+}
