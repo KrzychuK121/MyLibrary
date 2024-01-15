53647f7382ae5e676d11c6655f710d0d643c4d26
KrzychuK121 <KrzychuK1211@gmail.com>
2024-01-15 19:17:16 +0100

    Creating migrations for changing publishers name length (it was too short)

---
+++ MojaBiblioteka/Migrations/20240115181514_PublisherNameLength55.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MojaBiblioteka.Migrations
+{
+    public partial class PublisherNameLength55 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Publishers",
+                type: "nvarchar(55)",
+                maxLength: 55,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Publishers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(55)",
+                oldMaxLength: 55);
+        }
+    }
+}
