6f665307523cc845dd8d2c468eaedb7462e577a0
Komputer Domowy <KrzychuK1211@gmail.com>
2024-01-29 21:15:46 +0100

    Now Index action will show warning messages when logged in client has books with due date less or equal 7 days from when he is logged in

--- MojaBiblioteka/Controllers/HomeController.cs
+++ MojaBiblioteka/Controllers/HomeController.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MojaBiblioteka.Data;
 using MojaBiblioteka.Models;
+using MojaBiblioteka.Models.Entities.Connector;
+using MojaBiblioteka.Models.ViewModels.Messages;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Security.Claims;
 
 namespace MojaBiblioteka.Controllers
 {
@@ -9,14 +17,73 @@ namespace MojaBiblioteka.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MyLibraryContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController
+        (
+            ILogger<HomeController> logger,
+            MyLibraryContext context
+        )
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+
+            if (!User.IsInRole("Client"))
+                return View();
+
+            //rt.DueDate.Subtract(DateTime.Now.AtMidnight()).TotalDays <= 0
+
+            var ifSoonExpire = false;
+            var ifExpierd = false;
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var dueDates = _context.RentalTransactionList
+                .Where
+                (
+                    rt => rt.UserId.Equals(userId) &&
+                    rt.Status != (int) BookStatus.Cancelled &&
+                    rt.Status != (int) BookStatus.Returned &&
+                    rt.Status != (int) BookStatus.Closed
+                )
+                .Select(rt => rt.DueDate);
+
+            if (!dueDates.Any())
+                return View();
+
+            foreach(var dueDate in dueDates) 
+            {
+                if
+                (
+                    !ifSoonExpire &&
+                    dueDate.Subtract(DateTime.Now.AtMidnight()).TotalDays <= 7 &&
+                    dueDate.Subtract(DateTime.Now.AtMidnight()).TotalDays > 0
+                )
+                    ifSoonExpire = true;
+
+                if(!ifExpierd && dueDate.Subtract(DateTime.Now.AtMidnight()).TotalDays <= 0)
+                    ifExpierd = true;
+            }
+
+            var linkMessage = " Sprawdź które to książki klikając \"Moje wypożyczenia\" ";
+
+
+            if (ifSoonExpire)
+            {
+                ViewData["messageSoonExpire"] = "Niedługo termin Twoich zamówień dobiegnie końca." + linkMessage;
+                ViewData["typeSoonExpire"] = Types.Warning;
+            }
+
+            if (ifExpierd)
+            {
+                ViewData["messageExpired"] = "Posiadasz książki, które powinieneś już zwrócić." + linkMessage;
+                ViewData["typeExpired"] = Types.Error;
+            }
+
             return View();
         }
 
